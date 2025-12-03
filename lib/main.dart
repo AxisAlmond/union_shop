947cb73c6a70ed512f37935e6b59ed59bac6e9b2
AxisAlmond <almoneda.alecxis@gmail.com>
2025-12-03 15:36:57 +0000

    Updated main.dart to import & use footer_widget dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/product_page.dart';
+import 'package:union_shop/widgets/footer_widget.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -285,19 +286,7 @@ class HomeScreen extends StatelessWidget {
             ),
 
             // Footer
-            Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Text(
-                'Placeholder Footer',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+            const Footer(),
           ],
         ),
       ),
