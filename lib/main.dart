9827199cdd17897a0c36ac006571cb435afbbd8c
AxisAlmond <almoneda.alecxis@gmail.com>
2025-12-03 15:52:16 +0000

    Updated product_page.dart to import & use footer.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/product_page.dart';
-import 'package:union_shop/widgets/footer_widget.dart';
+import 'package:union_shop/widgets/footer.dart';
 
 void main() {
   runApp(const UnionShopApp());
