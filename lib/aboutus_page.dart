910ed10f53cc70b73887ce565c1f4c9fc7bdb5b2
AxisAlmond <almoneda.alecxis@gmail.com>
2025-12-04 14:56:18 +0000

    Updated aboutus_page.dart text to include format & style

--- lib/aboutus_page.dart
+++ lib/aboutus_page.dart
@@ -14,29 +14,47 @@ class AboutUs extends StatelessWidget {
           children: [
             Head(),
             Padding(
-              padding: EdgeInsetsGeometry.all(16),
-              child: Text('Welcome to the Union Shop!',
-              textAlign: TextAlign.center,),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.all(16),
-              child: Text(
-                'We’re dedicated to giving you the very best ' +
-                    'University branded products, with a range of clothing and' +
-                    'merchandise available to shop all year round! We even offer ' +
-                    ' an exclusive personalisation service!',)
-            ),
+                padding: EdgeInsetsGeometry.all(16),
+                child: Text('Welcome to the Union Shop!',
+                    textAlign: TextAlign.center,
+                    textScaler: TextScaler.linear(3),
+                    style: TextStyle(fontWeight: FontWeight.bold))),
             Padding(
                 padding: EdgeInsetsGeometry.all(16),
-                child: Text('All online purchases are available ' +
-                    'for delivery or instore collection!')
-                    ),
+                child: Text(
+                    'We’re dedicated to giving you the very best ' +
+                        'University branded products, with a range of clothing and ' +
+                        'merchandise available to shop all year round! We even offer ' +
+                        ' an exclusive personalisation service!',
+                    textAlign: TextAlign.center,
+                    textScaler: TextScaler.linear(1.33))),
             Padding(
                 padding: EdgeInsetsGeometry.all(16),
-                child: Text('We hope you enjoy our products as much as we enjoy ' +
-                    'offering them to you. If you have any questions or comments, ' +
-                    'please don’t hesitate to contact us at hello@upsu.net.')
-                    ),
+                child: Text(
+                    'All online purchases are available ' +
+                        'for delivery or instore collection!',
+                    textAlign: TextAlign.center,
+                    textScaler: TextScaler.linear(1.33))),
+            Padding(
+                padding: EdgeInsetsGeometry.all(16),
+                child: Text(
+                    'We hope you enjoy our products as much as we enjoy ' +
+                        'offering them to you. If you have any questions or comments, ' +
+                        'please don’t hesitate to contact us at hello@upsu.net.',
+                    textAlign: TextAlign.center,
+                    textScaler: TextScaler.linear(1.33))),
+            Padding(
+                padding: EdgeInsetsGeometry.all(16),
+                child: Text(
+                  'Happy Shopping!',
+                    textAlign: TextAlign.center,
+                    textScaler: TextScaler.linear(1.33))),
+            Padding(
+                padding: EdgeInsetsGeometry.all(16),
+                child: Text(
+                    'The Union Shop & Reception Team​​​​​​​​​',
+                    textAlign: TextAlign.center,
+                    textScaler: TextScaler.linear(1.33))),
             Footer(),
           ],
         ),
