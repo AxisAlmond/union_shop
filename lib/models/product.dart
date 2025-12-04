206e1459dcdb1f41ac48461e4fda205568232ede
AxisAlmond <almoneda.alecxis@gmail.com>
2025-12-04 18:18:57 +0000

    Created product.dart ready for collection

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String title;
+  final double price;
+  final String imagePath;
+  final bool isPopular;
+
+  Product({
+    required this.title,
+    required this.price,
+    required this.imagePath,
+    required this.isPopular,
+  });
+}
